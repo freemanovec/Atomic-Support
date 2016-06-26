c8fca62d030e38f5cfc901e9bbed1032e3241885
Jakub Rohla <Jakub Rohla>
2016-06-26 22:07:28 +0200

    Rename the project

---
+++ src/main/java/com/freemanovci/healer/Main.java
@@ -0,0 +1,18 @@
+package com.freemanovci.healer;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "healer";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
