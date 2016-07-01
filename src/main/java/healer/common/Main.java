18e3ed405c5fc02f3f3f0b6fa34cfd1755c5d96c
Jakub Rohla <freemanovec@gmail.com>
2016-07-01 17:46:09 +0200

    Created C# app to easily build new versions. It also adds the resources automatically.

--- src/main/java/healer/common/Main.java
+++ src/main/java/healer/common/Main.java
@@ -22,7 +22,7 @@ import mekanism.common.*;
 public class Main
 {
     public static final String MODID = "healer";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
